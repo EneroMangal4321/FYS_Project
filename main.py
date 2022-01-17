0bda058ec599ffc8daf1eaec519897be9bede97a
EneroMangal <enero.mangal@gmail.com>
2022-01-17 17:48:54 +0100

    update
    login werkend op officiele website

--- main.py
+++ main.py
@@ -12,15 +12,51 @@ app.config['MYSQL_DB'] = 'fys'
 
 mysql = MySQL(app)
 
-@app.route('/', methods=['GET', 'POST'])
-def home_pagina():
-    error = None
-    if request.method == 'POST':
-        if request.form['Surname'] != 'admin' or request.form['Ticketnumber'] != 'admin':
-            error = 'Invalid Surname and/or Ticketnumber, try again.'
+# @app.route('/', methods=['GET', 'POST'])
+# def home_pagina():
+#     error = None
+#     if request.method == 'POST':
+#         if request.form['Surname'] != 'admin' or request.form['Ticketnumber'] != 'admin':
+#             error = 'Invalid Surname and/or Ticketnumber, try again.'
+#         else:
+#             return {{url_for('wifi_verbinding')}}
+#     return render_template('/index.html', error=error)
+
+@app.route("/", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        email = request.form['email']
+        ticketnr = request.form['ticketnr']
+
+        email = f"'{email}'"
+        # get id of costumer using email name as citeria from db.
+        cur = mysql.connection.cursor()
+        cur.execute("""SELECT id FROM Klanten WHERE email = %s""" % (str(email)))
+        klant_id_Klanten = cur.fetchall()
+        cur.close()
+
+        # get klantid from db using ticketnumber as criteria.
+        cur = mysql.connection.cursor()
+        cur.execute("""SELECT klantid FROM Ticket WHERE id = %s""" % (int(ticketnr)))
+        klant_id_Ticket = cur.fetchall()
+        cur.close()
+
+        # check if the klantid is the same as the id of the Klanten table.
+        if klant_id_Klanten == klant_id_Ticket:
+            # print(">", type(klant_id_Ticket))
+            # print(klant_id_Ticket[0][0])
+            klant_id_Ticket = int(klant_id_Ticket[0][0])
+            cur = mysql.connection.cursor()
+            cur.execute("""SELECT voornaam FROM Klanten WHERE id = %s""" % (int(klant_id_Ticket)))
+            fname = cur.fetchall()
+            cur.close()
+            return render_template("WiFi_verbinding.html", fname = fname[0][0])
+        
         else:
-            return {{url_for('wifi_verbinding')}}
-    return render_template('/index.html', error=error)
+            print("Incorrecte naam of ticketnummer")
+            return  render_template("index.html")
+    
+    return  render_template("index.html")
 
 
 @app.route('/WiFi_verbinding.html', methods=['GET', 'POST'])
