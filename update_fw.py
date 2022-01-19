f7f5d45611240d7d59f7245b5488b82bc78f46a2
EneroMangal <enero.mangal@gmail.com>
2022-01-19 19:16:17 +0100

    update

--- update_fw.py
+++ update_fw.py
@@ -2,10 +2,13 @@ import subprocess
 
 correct_user = "enero"
 
+# sudo apt-get install iptables-persistent
 def rerouteRequest():
     output = subprocess.getoutput("iptables -t nat -A PREROUTING -i wlan0ap -p tcp --dport 80 -j DNAT  --to-destination  192.168.1.99:80")
-    save_output = subprocess.getoutput("insert save iptable rules")
+    output1 = subprocess.getoutput("iptables -t nat -A PREROUTING -i wlan0ap -p tcp --dport 443 -j DNAT  --to-destination  192.168.1.99:80")
+    save_output = subprocess.getoutput("sudo /etc/init.d/iptables-persistent save")
     print(output)
+    print(output1)
     print(save_output)
     print("OK")
 
